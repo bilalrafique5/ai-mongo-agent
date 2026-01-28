6d5f5c1b86342b5c2ac37a3048e0c2145bed3f2a
Bilal-Rafique-coder <chbilalrafique2@gmail.com>
2026-01-28 21:42:44 +0500

    updated with collections settings

--- app/main.py
+++ app/main.py
@@ -7,7 +7,10 @@ load_dotenv()
 
 app=FastAPI(title="AI MongoDB Agent")
 
+# AUTH ROUTES
 app.include_router(auth_routes.router)
+# PERSON ROUTES
 app.include_router(person_routes.router)
+# AI ROUTES
 app.include_router(ai_routes.router)
 
